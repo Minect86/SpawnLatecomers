4d78c9b414b13206d7cbce5ffbf1163429d0ad33
Minect86 <kobzeve668@gmail.com>
2025-02-28 12:20:25 +0300

    Добавьте файлы проекта.

---
+++ SpawnLatecomers/Config.cs
@@ -0,0 +1,19 @@
+﻿using Exiled.API.Interfaces;
+using PlayerRoles;
+using System.Collections.Generic;
+
+namespace SpawnLatecomers
+{
+    internal class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        public bool Debug { get; set; } = false;
+        public float TimeInSeconds { get; set; } = 60;
+        public List<RoleTypeId> RoleTypes { get; set; } = new List<RoleTypeId>()
+        {
+            RoleTypeId.FacilityGuard,
+            RoleTypeId.ClassD,
+            RoleTypeId.Scientist
+        };
+    }
+}
---
+++ SpawnLatecomers/Plugin.cs
@@ -0,0 +1,47 @@
+﻿using Exiled.API.Features;
+using Exiled.Events.EventArgs.Player;
+using MEC;
+using System;
+using System.Collections.Generic;
+
+namespace SpawnLatecomers
+{
+    internal class Plugin : Plugin<Config>
+    {
+        public override void OnEnabled()
+        {
+            Exiled.Events.Handlers.Player.Joined += PlayerJoin;
+            base.OnEnabled();
+        }
+        public override void OnDisabled()
+        {
+            Exiled.Events.Handlers.Player.Joined -= PlayerJoin;
+            base.OnDisabled();
+        }
+
+        private Random rnd = new Random();
+        private HashSet<Player> joinedplayers = new HashSet<Player>();
+
+        private void PlayerJoin(JoinedEventArgs ev)
+        {
+            if (Round.IsLobby)
+                joinedplayers.Add(ev.Player);
+
+            if (!Round.IsLobby && !joinedplayers.Contains(ev.Player) && (Round.ElapsedTime <= TimeSpan.FromSeconds(Config.TimeInSeconds)))
+            {
+                joinedplayers.Add(ev.Player);
+                SpawnPlayer(ev.Player);
+            }
+
+            Timing.CallDelayed(Config.TimeInSeconds + 1, () =>
+            {
+                joinedplayers.Clear();
+            });
+        }
+
+        private void SpawnPlayer(Player pl)
+        {
+            pl.Role.Set(Config.RoleTypes[rnd.Next(Config.RoleTypes.Count)]);
+        }
+    }
+}
